961054cf8a59c15dde56911bcd21a4d12f2efe1c
jinyoung <sm099@naver.com>
2022-07-06 12:10:37 +0900

    3. useState(part 1)

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+// useState import
+import { useState } from 'react';
 
 function App() {
   let title = 'JINY VLOG';
+  // state 생성
+  let [boardTitle] = useState(['오늘의 옷 추천','오늘의 맛집 추천', '오늘의 머리 추천']);
+  
   return (
     <div className="App">
       {/* nav */}
@@ -13,11 +18,29 @@ function App() {
       {/* end nav */}
 
       {/* title */}
-        {/* JSX 문법 3 : style = {{style명 : '스타일 값'}} */}
-      <section style={{display:'flex', justifyContent:'center', fontSize:'26px'}}>
+      {/* JSX 문법 3 : style = {{style명 : '스타일 값'}} */}
+      <section style={{display:'flex', justifyContent:'center', fontSize:'26px', marginBottom : '20px'}}>
         <h4>JINY LOG</h4>
       </section>
       {/* end title */}
+
+      {/* board layout */}
+      <section className="list">
+        {/* state binding */}
+        <h4>{boardTitle[0]}</h4>
+        <p>2월 17일</p>
+      </section>
+      <section className="list">
+        {/* state binding */}        
+        <h4>{boardTitle[1]}</h4>
+        <p>2월 17일</p>
+      </section>
+      <section className="list">
+        {/* state binding */}        
+        <h4>{boardTitle[2]}</h4>
+        <p>2월 17일</p>
+      </section>
+      {/* end board layout */}
     </div>
   );
 }

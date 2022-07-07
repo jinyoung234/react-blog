775658fbff9c0dbdc7c4e0413987e5a0e067688f
jinyoung <sm099@naver.com>
2022-07-08 02:53:48 +0900

    10. blog complete

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -7,21 +7,33 @@ import { useState } from 'react';
 function App() {
   let title = 'JINY VLOG';
   // board title state
-  let [boardTitle, modifyTitle] = useState(['옷 추천','맛집 추천', '머리 추천']);
-  let[titleState, setTitleState] = useState(0);
-
-  const modifyTitle1 = () => {
-    boardTitle[2] = 
-    modifyTitle(boardTitle[2])
-  }
+  let [boardTitle, modifyTitle] = useState([]);
+  
+  // 각각의 board title에 대한 state(title 클릭 시 각 title을 modal에 띄우기 위해)
+  let [titleState, setTitleState] = useState(0);
 
   // board title modify
   const modifyBoardTitle = () => {
     let copy = [...boardTitle];
     copy[2] = '여자 코트 추천';
-    console.log(copy);
     modifyTitle(copy);
-  }   
+  }
+  
+  // add board title
+  const addTitle = (inputState) => {
+    if((inputState !== '')) {
+      let copy = [...boardTitle];
+      copy.unshift(inputState);
+      modifyTitle(copy);
+    }
+  }
+
+  // delete board title
+  const deleteTitle = (index) => {
+    let copy = [...boardTitle];
+    copy.splice(index, 1);
+    modifyTitle(copy);
+  }
 
   // board title sort 
   const sortTitle = () => {
@@ -29,10 +41,27 @@ function App() {
     modifyTitle(copy);
   }
 
+  // weather state
+  let [weatherState, setWeatherState] = useState([]);
 
+  // add weatherState
+  const addWeatherState = (weatherState) => {
+    let copy = [...weatherState];
+    const date = new Date();
+    const dateData = date.toLocaleDateString('ko-kr');
+    copy.unshift(dateData);
+    setWeatherState(copy);
+  }
+
+  // delete WeatherState
+  const deleteWeatherState = (index) => {
+    let copy = [...weatherState];
+    copy.splice(index,1);
+    setWeatherState(copy);
+  }
 
   // heart state
-  let [heart, heartPlus] = useState([0,0,0]);
+  let [heart, heartPlus] = useState([]);
   
   // heart count
   const heartCount = (index) => {
@@ -41,8 +70,25 @@ function App() {
     heartPlus(copy);
   }
 
+  // add heart -> heart setState
+  const addHeart = (heart) => {
+    let copy = [...heart];
+    copy.unshift(0);
+    heartPlus(copy);
+  }
+
+  // delete heart -> heart setState
+  const deleteHeart = (index) => {
+    let copy = [...heart];
+    copy.splice(index, 1);
+    heartPlus(copy);
+  }
+
   // modal state (초기값 false로 설정하여 닫혀있는 상태)
   let [modal, setModal] = useState(false);
+
+  // input state
+  let [inputState, setInputState] = useState('');
 
   return (
     <div className="App">
@@ -67,14 +113,30 @@ function App() {
           return (
             <section className="list">
               {/* state binding */}
-              <h4 onClick={()=>{setModal(!modal); setTitleState(index) }}>{boardTitle[index]}</h4>
+              <h4 onClick={()=>{setModal(!modal); setTitleState(index) }}>{a}</h4>
               <span onClick={() => {heartCount(index)}}> ❤️ </span> {heart[index]}
-              <p>2월 17일</p>
+              <p>{weatherState[index]}</p>
+              <button onClick={()=>{deleteTitle(index); deleteHeart(index); deleteWeatherState(index);}} 
+                      style={{marginBottom:'15px'}}
+              >삭제</button>
             </section>
           )
         })
       }
       {/* end board component */}
+
+      {/* writing input */}
+      <div style={{marginTop:'20px', display:'flex', justifyContent:'center'}}>
+        <input 
+          onChange={(e)=>{ setInputState(e.target.value); }}
+          type={"text"}
+        ></input>
+        <button 
+          style={{marginLeft:'10px'}}
+          onClick={()=>{ addTitle(inputState); addHeart(heart); addWeatherState(weatherState)  }}
+        >add</button>
+      </div>
+      {/* end writing input */}
 
       {/* sortTitleButton */}
       <div 
@@ -88,7 +150,7 @@ function App() {
       {/* jsx의 조건부 바인딩 */}
       {
         // modal의 상태가 true일 때 Modal 등장
-        modal == true ? <Modal titleState={titleState} boardTitle={boardTitle}></Modal> : '' 
+        modal == true ? <Modal modifyTitle={modifyTitle} titleState={titleState} boardTitle={boardTitle}></Modal> : '' 
       }
     </div>
   );
@@ -96,13 +158,18 @@ function App() {
 
 // modal component
 function Modal(props){
+  const modifyBoardTitle = () => {
+    let copy = [...props.boardTitle];
+    copy[2] = '여자 코트 추천';
+    props.modifyTitle(copy);
+  }  
   return (
   <>
     <div className="modal">
       <p>{props.boardTitle[props.titleState]}</p>
       <p>이름</p>
       <p>날짜</p>
-      <button>글 수정</button>
+      <button onClick={modifyBoardTitle}>글 수정</button>
     </div>
   </>    
   )
